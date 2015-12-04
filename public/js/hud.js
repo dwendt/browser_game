9dac69b1c349f1820df641dd24f7f1ce1defe636
chris113113 <pirillo.chris@gmail.com>
2015-12-04 11:19:36 -0500

    Pulls html out into template in hud.js, adds css to main.css insteal of index.html

--- public/js/hud.js
+++ public/js/hud.js
@@ -8,6 +8,21 @@ define(['jquery','assets'], function($, Assets) {
   function Hud() {
     var self = this; // preserve this for callbacks
 
+    var mainHudBody = (""+
+    "<div id='hud'>"+
+      "<div id='health-container' class='hud health container'>"+
+        "<div id='health-text' class='health text'> 30 / 30 </div>"+
+        "<div id='health-current' class='health current'></div>"+
+      "</div>"+
+
+      "<div id='score-container' class='hud scorecontainer'></div>"+
+        "<div id='score' class='hud score'></div>"+
+      "</div>"+
+    "</div>"+
+    "");
+
+    $(document.body).append(mainHudBody);
+
     $('#hud').show();
 
   };
@@ -24,8 +39,8 @@ define(['jquery','assets'], function($, Assets) {
   };
 
   Hud.prototype.updateHealth = function(health, maxHealth) {
-    $('.health.current').innerWidth((100*health/maxHealth)+"%");
-    $('.health.text').text(health+" / "+maxHealth);
+    $('.health.current').innerWidth((100*health/maxHealth)+'%');
+    $('.health.text').text(health+' / '+maxHealth);
   };
 
 
